1a03ba403440474451baf72bacf44e890599e747
rmwirabua <rmwirabua@safaricom.co.ke>
2022-01-08 10:31:02 +0300

    created the jsquery scripts

--- js/exercises.js
+++ js/exercises.js
@@ -1,15 +1,15 @@
-// $(document).ready(){
-//   function fn1(){
-//   var nameInput = document.getElementById("name");
-//   var emailInput = document.getElementById("email");
-//   var inputedtext = document.getElementById("textarea");
-//  alert("Hi" + nameInput + "we have received your message. Thank you for reaching out to us")
+$(document).ready(function(){
+  $("img#img-des").click(function(){
+    $("p#wht-text0").toggle();
+    $("img#img-des").toggle();
+  });
+  $("img#img-dev").click(function(){
+    $("p#wht-text1").toggle();
+    $("img#img-dev").toggle();
+  });
+  $("img#img-product").click(function(){
+    $("p#wht-text2").toggle();
+    $("img#img-product").toggle();
+  });
 
-
-
-
-//   });
-// });
-$(document).ready(){
-  $("button#")
-}
+});
